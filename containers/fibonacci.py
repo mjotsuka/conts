a64d520739b0878e40d296f52329d1a13203bb36
Megan Otsuka <motsuka@students.pitzer.edu>
2021-04-01 10:46:38 -0700

    fixed functions so test cases pass

--- containers/fibonacci.py
+++ containers/fibonacci.py
@@ -1,13 +1,13 @@
 
-################################################################################
+##############################################################################
 # example fibonacci number code;
 # you do not have to modify this code in any way
-################################################################################
+##############################################################################
 
 
 def fibs(n):
     '''
-    This function computes the first n fibonacci numbers.
+    This function computes the f1 n fibonacci numbers.
     Notice that this function uses O(n) memory.
     '''
     fibs = []
@@ -46,10 +46,10 @@ def fib(n):
     return f2
 
 
-################################################################################
+##############################################################################
 # fibonacci number code using generators;
 # you will need to implement the functions below
-################################################################################
+##############################################################################
 
 
 class Fib:
@@ -61,15 +61,70 @@ class Fib:
     [1, 1, 2, 3, 5]
     '''
 
+    def __init__(self, n=None):
+        self.n = n
+
+    def __repr__(self):
+        if self.n is None:
+            return "Fib()"
+        else:
+            return "Fib(" + str(self.n) + ")"
+
+    def __iter__(self):
+        return FibIter(self.n)
+
 
 class FibIter:
     '''
     This is the iterator helper class for the Fib class.
     '''
 
+    def __init__(self, n):
+        self.n = n
+        self.count = 0
+        self.f1 = 1
+        self.f2 = 1
+        self.f3 = None
+
+    def __next__(self):
+        if self.n and self.n <= self.count:
+            raise StopIteration
+        elif self.count < 2:
+            self.count += 1
+            return 1
+        else:
+            self.count += 1
+            self.f3 = self.f1 + self.f2
+            self.f1 = self.f2
+            self.f2 = self.f3
+            return self.f3
+
 
 def fib_yield(n=None):
     '''
-    This function returns a generator that computes the first n fibonacci numbers.
+    This function returns a generator that computes the f1 n fibonacci numbers.
     If n is None, then the generator is infinite.
     '''
+
+    f0 = 1
+    f1 = 1
+
+    if n is None:
+        yield f0
+        yield f1
+        while True:
+            f2 = f1 + f0
+            f0 = f1
+            f1 = f2
+            yield f2
+    if n == 1:
+        yield 1
+    else:
+        yield f0
+        yield f1
+        while (n - 2) > 0:
+            f2 = f1 + f0
+            f0 = f1
+            f1 = f2
+            yield f2
+            n -= 1
--- containers/range.py
+++ containers/range.py
@@ -11,19 +11,62 @@ def range(a, b=None, c=None):
     [1, 3]
 
     HINT:
-    If you can figure out how to use the built-in range function (without modifying the test cases!),
+    If you can figure out how to use the built-in range function
+    (without modifying the test cases!),
     then feel free to do so.
-    That's fairly difficult to do, however, and it's much easier to just implement this function normally using the yield syntax.
+    That's fairly difficult to do, however, and it's much easier to
+    just implement this function normally using the yield syntax.
 
     NOTE:
-    For efficiency reasons, Python's built-in range object is written in the C programming language rather than natively in python.
-    You can find the source code online at https://hg.python.org/cpython/file/ee7b713fec71/Objects/rangeobject.c
+    For efficiency reasons, Python's built-in range object is written in
+    the C programming language rather than natively in python.
+    You can find the source code online at
+    https://hg.python.org/cpython/file/ee7b713fec71/Objects/rangeobject.c
     The link takes you to a file that is 1445 lines long,
     and all it does is implement this simple functionality.
-    My easy to read Python implementation of this function is just 15 lines long.
-    (And with some code golf I also wrote a less readable version that is only 4 lines.)
-    It is very common for C programs to be 100x longer than their corresponding python programs.
-    C code must manage lots of details about the computer manually that python code automates for you.
-    Carefully written C code can be faster than the corresponding python code because it can remove some of the overhead of this automation process,
+    My easy to read Python implementation of this function is just
+    15 lines long.
+    (And with some code golf I also wrote a less readable version
+    that is only 4 lines.)
+    It is very common for C programs to be 100x longer than their
+    corresponding python programs.
+    C code must manage lots of details about the computer manually that
+    python code automates for you.
+    Carefully written C code can be faster than the corresponding python
+    code because it can remove some of the overhead of this automation process,
     but the resulting code is much longer and harder to read/write.
     '''
+
+    if b and c:
+        if c > b and b > 0:
+            yield 0
+        elif b / c < 0:
+            return []
+        elif b < 0 and c < 0:
+            z1 = 0
+            while z1 > b:
+                yield z1
+                z1 += c
+        else:
+            z1 = 0
+            while z1 < b:
+                yield z1
+                z1 += c
+
+    elif c is None and b:
+        if a >= b:
+            return []
+        else:
+            y1 = a
+            while y1 < b:
+                yield y1
+                y1 += 1
+
+    elif b is None and c is None:
+        if a <= 0:
+            return []
+        else:
+            x1 = 0
+            while x1 < a:
+                yield x1
+                x1 += 1
